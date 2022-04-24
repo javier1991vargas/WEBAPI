18ce918d53a2e2d147c91ad248846c51bc3a2a42
javier1991vargas <104287410+javier1991vargas@users.noreply.github.com>
2022-04-24 12:51:40 -0500

    Add files via upload

---
+++ StarUp.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System.Reflection.Metadata;
+using System.Text.Json.Serialization;
+
+namespace WebApAutores
+{
+    public class StarUp
+    {
+
+        public StarUp(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers().AddJsonOptions(x =>
+            x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+            //       services.AddControllers().AddNewtonsoftJson(x =>
+
+            //x.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("defaultConnection")));
+            
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddAutoMapper(typeof(StarUp));
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(Endpoint=>
+            {
+                Endpoint.MapControllers();
+            });
+
+
+        }
+    }
+}
---
+++ WebApAutores/Controllers/ComentariosController.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApAutores.DTOs;
+using WebApAutores.Entidades;
+
+namespace WebApAutores.Controllers
+{
+    [ApiController]
+    [Route("api/libros/{LibroId:int}")]
+    public class ComentariosController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        private readonly IMapper mapper;
+
+        public ComentariosController(ApplicationDbContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<ComentarioDTO>>> Get(int _libroId)
+        {
+            var existe = await context.Comentarios.AnyAsync(x => x.LibroID == _libroId);
+
+            if (!existe)
+            {
+                return NotFound();
+            }
+            var comentarios = await context.Comentarios.Where(x => x.LibroID == _libroId).ToListAsync();
+            return mapper.Map<List<ComentarioDTO>>(comentarios);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(int _libroId, ComentariosCreacionDTO _comentariosCreacionDTO)
+        {
+            var existe = await context.Libros.AnyAsync(x => x.Id == _libroId);
+
+            if (!existe)
+            {
+                return NotFound();
+            }
+            var comentario_ = mapper.Map<Comentarios>(_comentariosCreacionDTO);
+            comentario_.LibroID = _libroId;
+            context.Add(comentario_);
+            await context.SaveChangesAsync();
+            return Ok();
+
+        }
+
+    }
+}
+
